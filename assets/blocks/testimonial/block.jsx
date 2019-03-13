fc1cef94325d02358da98b81dd0b179964c903c9
Linh LV <linh.codeunited@gmail.com>
2019-03-13 11:13:27 +0700

    Apply icon to backend editor

--- assets/blocks/testimonial/block.jsx
+++ assets/blocks/testimonial/block.jsx
@@ -47,8 +47,8 @@
                     centerMode: !avatarBottom,
                     centerPadding: '40px',
                     slidesToShow: num,
-                    nextArrow: !!nextArrow ? nextArrow : undefined, //<div class="slick-next"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="none" d="M0 0h24v24H0V0z"/><path d="M12 4l-1.41 1.41L16.17 11H4v2h12.17l-5.58 5.59L12 20l8-8-8-8z"/></svg></div>
-                    prevArrow: !!prevArrow ? prevArrow : undefined, //<div class="slick-prev"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M0 0h24v24H0z" fill="none"/><path d="M20 11H7.83l5.59-5.59L12 4l-8 8 8 8 1.41-1.41L7.83 13H20v-2z"/></svg></div>
+                    nextArrow: !!nextArrow ? `<button class="advgb-arrow advgb-next"><img src="${nextArrow}" alt="Next" /></button>` : undefined, //<div class="slick-next"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="none" d="M0 0h24v24H0V0z"/><path d="M12 4l-1.41 1.41L16.17 11H4v2h12.17l-5.58 5.59L12 20l8-8-8-8z"/></svg></div>
+                    prevArrow: !!prevArrow ? `<button class="advgb-arrow advgb-prev"><img src="${prevArrow}" alt="Prev" /></button>` : undefined, //<div class="slick-prev"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M0 0h24v24H0z" fill="none"/><path d="M20 11H7.83l5.59-5.59L12 4l-8 8 8 8 1.41-1.41L7.83 13H20v-2z"/></svg></div>
                 });
             }
         }
@@ -90,8 +90,8 @@
                         centerMode: !avatarBottom,
                         centerPadding: '40px',
                         slidesToShow: num,
-                        nextArrow: !!nextArrow ? nextArrow : undefined,
-                        prevArrow: !!prevArrow ? prevArrow : undefined,
+                        nextArrow: !!nextArrow ? `<button class="advgb-arrow advgb-next"><img src="${nextArrow}" alt="Next" /></button>` : undefined,
+                        prevArrow: !!prevArrow ? `<button class="advgb-arrow advgb-prev"><img src="${prevArrow}" alt="Prev" /></button>` : undefined,
                     });
                 }
             }
