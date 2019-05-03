3ca861ede06106db1af13e9ec4c9dbcf758ecf3d
Linh LV <linh.codeunited@gmail.com>
2019-05-03 09:47:00 +0700

    Fix gallery layout on first load

--- assets/blocks/advgallery/gallery-loadmore.js
+++ assets/blocks/advgallery/gallery-loadmore.js
@@ -6,9 +6,9 @@ jQuery(document).ready(function ($) {
             columnWidth: '.advgb-gallery-sizer',
             percentPosition: true
         });
-        $masonryWrapper.on('load', function () {
+        $masonryWrapper.imagesLoaded().progress( function() {
             $masonryWrapper.masonry('layout');
-        })
+        });
     }
 
     $('.advgb-gallery-container .advgb-load-more').find('a').click(function (e) {
